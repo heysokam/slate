72d1060ff22d396f712e9225f44e0095605d547b
sOkam <7308253+heysokam@users.noreply.github.com>
2024-06-09 10:41:10 +0200

    rew: Extras: Support for `{.stub.}` object types

--- src/slate/element/extras.nim
+++ src/slate/element/extras.nim
@@ -4,19 +4,24 @@
 # @fileoverview Syntax rules that are not natively defined by Nim
 #__________________________________________________________________|
 # @deps *Slate
+import ../errors
 import ../nimc as nim
 import ./general
 
 
 #_______________________________________
-# @section Extra Tools
+# @section Extras: Calls
 #_____________________________
 func isSpecialCall *(code :PNode) :bool=
   ## @descr Returns true if the {@arg code} meets the conditions to be a Special Call
   if code.kind != nkCall : return false  # Only Calls can be special calls
   if code.len < 2        : return false  # Calls without arguments cannot be special calls
   return true
-#___________________
+
+
+#_______________________________________
+# @section Extras: Objects
+#_____________________________
 const ValidUnionInfixes = [".:"]
 proc isUnion *(code :PNode) :bool=
   ## @descr Returns true if the {@arg code} describes a Union initializer special case
@@ -36,4 +41,30 @@ proc isObjConstr *(code :PNode; skipObjConstr :bool= false) :bool=
   if code[Arg0].kind == nkIdent and
      code[Arg0].strValue in ValidObjConstrSymbols:
     return true  # StructType(_) special case
+
+
+#_______________________________________
+# @section Extra Node Properties: Types
+#_____________________________
+proc isStubInherit *(code :PNode) :bool=
+  ## @descr Returns true if the given {@arg code} meets all the conditions to be a stub inherit object type
+  const (Name,Inherit,Type, Pragma,Body) = (0,1,^1, ^1,^1)
+  code.kind == nkObjectTy and
+  code[Inherit].kind == nkOfInherit and
+  code[Inherit][Name].kind == nkIdent and
+  code[Body].kind == nkEmpty
+#___________________
+proc isStub *(code :PNode) :bool=
+  ## @descr
+  ##  Returns true if the given {@arg code} meets all the conditions to be a stub object typedef
+  ##  1. Code has to be an Object Typedef
+  ##  2. Its name must be a pragma
+  ##  3. Its first pragma must be "stub"
+  ##  4. It must inherit from an indentifier `object of thing`
+  ##  5. Its body must be empty
+  const (Name,Inherit,Type, Pragma,Body) = (0,1,^1, ^1,^1)
+  case code.kind
+  of nkTypeDef  : return code[Type].kind == nkObjectTy and code[Name].hasStubPragma and code[Type].isStubInherit
+  of nkObjectTy : return code.isStubInherit
+  else: code.err "Tried to get the stub condition of an unmapped node kind."
 
