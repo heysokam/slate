15fafe84aab56c7650f13b9a5bc0546ab8863910
sOkam <7308253+heysokam@users.noreply.github.com>
2024-07-05 21:21:50 +0200

    chg: Better `C.Ast` formatting and tools

--- src/slate/C/ast.zig
+++ src/slate/C/ast.zig
@@ -4,6 +4,9 @@
 pub const Ast = @This();
 // @deps std
 const std = @import("std");
+// @deps zstd
+const zstd = @import("../../zstd.zig");
+const Seq  = zstd.Seq;
 // @deps minim
 pub usingnamespace @import("./rules.zig");
 pub const Node   = @import("./node.zig").Node;
@@ -11,11 +14,25 @@ pub const Node   = @import("./node.zig").Node;
 /// @descr Contains the list of Top-Level nodes of the AST
 list  :Node.List,
 
+/// @descr Returns an empty {@link Ast} object
+pub fn newEmpty() Ast { return Ast{.list = Ast.Node.List{}}; }
+/// @descr Creates a new empty {@link Ast} object with its data correctly initialized.
+pub fn create(A :std.mem.Allocator) Ast { return Ast{.list= Node.List.create(A)}; }
+/// @descr Releases all memory used by the Node.List
+pub fn destroy(L:*Ast) void { L.list.destroy(); }
 /// @descr Returns true if the AST has no nodes in its {@link Ast.list} field
 pub fn empty(ast :*const Ast) bool { return ast.list.empty(); }
+/// @descr Adds the {@arg val} Node to the Node.List of the {@arg ast}.
+pub fn append(ast :*Ast, val :Node) !void { try ast.list.append(val); }
 
 pub fn format(ast :*const Ast, comptime _:[]const u8, _:std.fmt.FormatOptions, writer :anytype) !void {
   if (ast.empty()) return;
-  try writer.print("", .{});
+  for (ast.list.data.?.items) | N | { try writer.print("{s}", .{N}); }
+}
+
+pub fn report(ast :*const Ast) void {
+  std.debug.print("..*Slate.C.Codegen.........\n", .{});
+  std.debug.print("{s}", .{ast});
+  std.debug.print("...........................\n", .{});
 }
 
--- src/slate/C/node.zig
+++ src/slate/C/node.zig
@@ -1,10 +1,13 @@
 //:___________________________________________________________________
 //  *Slate  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
 //:___________________________________________________________________
+// @deps std
+const std = @import("std");
 // @deps zstd
 const zstd = @import("../../zstd.zig");
+const fail = zstd.fail;
 const Seq  = zstd.Seq;
-// @deps minim.ast
+// @deps C.ast
 const Func = @import("./func.zig");
 
 
@@ -14,8 +17,21 @@ pub const Node = union(enum) {
   pub const List = struct {
     data :?Seq(Node)= null,
 
+    /// @descr Creates a new empty Node.List object.
+    pub fn create(A :std.mem.Allocator) Node.List { return Node.List{.data= Seq(Node).init(A)}; }
+    /// @descr Releases all memory used by the Node.List
+    pub fn destroy(L:*Node.List) void { L.data.?.deinit(); }
     /// @descr Returns true if the Node list has no nodes.
     pub fn empty(L:*const Node.List) bool { return L.data == null or L.data.?.items.len == 0; }
+    /// @descr Adds the {@arg val} Node to the Node.List of the {@arg ast}.
+    pub fn append(L :*Node.List, val :Node) !void { try L.data.?.append(val); }
   };
+
+  pub fn format(N :*const Node, comptime _:[]const u8, _:std.fmt.FormatOptions, writer :anytype) !void {
+    switch (N.*) {
+    .Func => try writer.print("{s}", .{N.*.Func}),
+    // else  => |node| fail("Unknown C.Node Kind '{s}'", .{@tagName(node)})
+    }
+  }
 };
 
