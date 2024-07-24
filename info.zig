0a2f3e18e726ee0c2eac1ce4ad5d760ab9a3242d
sOkam <7308253+heysokam@users.noreply.github.com>
2024-07-24 10:45:58 +0200

    git: Update library metadata at `info.zig`

--- info.zig
+++ info.zig
@@ -1,1 +1,8 @@
-const version = "0.6.1";
+//:___________________________________________________________________
+//  *Slate  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
+//:___________________________________________________________________
+const version      = "0.6.1";
+const name         = "slate";
+const author       = "sOkam";
+const description  = "*Slate | StoS Compiler Helper for Nim syntax";
+const license      = "LGPLv3-or-later";
