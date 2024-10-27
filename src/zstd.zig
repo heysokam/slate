193f157b05b40833eaa3e219977249765303fefe
sOkam <7308253+heysokam@users.noreply.github.com>
2024-10-27 15:02:29 +0100

    new: Add `UnorderedSet` to the zstd clone

--- src/zstd.zig
+++ src/zstd.zig
@@ -44,3 +44,27 @@ pub const files = struct {
   }
 };
 
+//______________________________________
+// @section Set Aliases
+//____________________________
+/// @descr Describes a set[T] that doesn't maintain insertion order.
+/// @todo Support for containing strings by conditionally returning an std.BufSet
+/// @todo Rename to UnorderedSet and implement set as OrderedSet by default
+pub const set = struct {
+  pub fn Unordered (comptime T :type) type {
+  // pub fn set (comptime T :type) type {
+    return struct {
+      data  :Data,
+      const Data = std.AutoHashMap(T, void);
+      pub const Iter = Data.KeyIterator;
+      pub fn create   (A :std.mem.Allocator) @This() { return @This(){.data= std.AutoHashMap(T, void).init(A)}; }
+      pub fn destroy  (S :*@This()) void { S.data.deinit(); }
+      pub fn incl     (S :*@This(), val :T) !void { _ = try S.data.getOrPut(val); }
+      pub fn excl     (S :*@This(), val :T)  void { _ = S.data.remove(val) ; }
+      pub fn iter     (S :*const @This()) @This().Iter { return S.data.keyIterator(); }
+      pub fn contains (S :*const @This(), val :T) bool { return S.data.contains(val); }
+      pub fn has      (S :*const @This(), val :T) bool { return S.contains(val); }
+    };
+  }
+};
+
