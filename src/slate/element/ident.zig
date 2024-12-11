f9e7cba15763bd3dc9fe1a5b568a9adbb2155e95
sOkam <7308253+heysokam@users.noreply.github.com>
2024-12-11 13:44:00 +0100

    fmt: `Ident.from` now uses `source.Loc.from` as it should have

--- src/slate/element/ident.zig
+++ src/slate/element/ident.zig
@@ -12,4 +12,4 @@ const source = @import("../source.zig").source;
 /// @descr Describes the location in the source code of the name used to access the target entity
 name  :source.Loc,
 
-pub fn from (I:*const Ident, src :source.Code) source.Code { return src[I.name.start..I.name.end]; }
+pub fn from (I:*const Ident, src :source.Code) source.Code { return I.name.from(src); }
