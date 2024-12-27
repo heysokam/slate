6b193dca1de11fcb0db94898b23dfb1b2b300232
sOkam <7308253+heysokam@users.noreply.github.com>
2024-12-27 08:12:40 +0100

    bld: Skip tests for `gen/C/proc` (temp)
    Will cleanup the file when development on runic continues and split into
    multiple files.
    Need to solve the minim parser first.

--- src/slate/gen/C/proc.test.zig
+++ src/slate/gen/C/proc.test.zig
@@ -1,7 +1,7 @@
 //:___________________________________________________________________
 //  *Slate  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
 //:___________________________________________________________________
-//! @fileoverview Unit Tests for slate/gen/C/proc.test.zig
+//! @fileoverview Unit Tests for slate/gen/C/proc.zig
 //____________________________________________________________|
-test "[TODO]"
-{ @import("std").debug.print("[slate.tests] TODO: slate/gen/C/proc.test.zig has no tests.\n", .{}); }
+test "TODO: slate/gen/C/proc.test.zig has no tests.\n"
+{ return error.SkipZigTest; }
