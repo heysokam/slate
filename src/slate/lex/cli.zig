826664713b3d5f500100e62a81ec9febd4caba81
sOkam <7308253+heysokam@users.noreply.github.com>
2025-04-23 08:18:24 +0200

    fix: Lexer.report not printing the last character

--- src/slate/lex/cli.zig
+++ src/slate/lex/cli.zig
@@ -9,8 +9,8 @@ const Lex = @import("../lexer.zig").Lex;
 
 pub fn report(L:*Lex) void {
   Lex.prnt("--- slate.Lexer ---\n", .{});
-  for (L.res.items(.id), L.res.items(.loc)) | id, loc | {
-    Lex.prnt("{s} : {s}\n", .{@tagName(id), L.src[loc.start..loc.end]});
+  for (L.res.items(.id), L.res.items(.loc)) |id, loc| {
+    Lex.prnt("{s} : {s}\n", .{@tagName(id), loc.from(L.src)});
   }
   Lex.prnt("-------------------\n", .{});
 }
