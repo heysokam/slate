825eb362c8b9ff6a4ff20a64e6bc260acfb3adde
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-25 14:13:34 +0200

    fmt: Move nim grammar and lexer into nim/slate subfolder

--- src/nim/lexer/info.nim
+++
@@ -1,8 +0,0 @@
-#:__________________________________________________________________
-#  lex  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
-#:__________________________________________________________________
-const version     * = "0.0.0"
-const name        * = "lexbase"
-const description * = "Lex.base | Nim Base Lexer Test"
-const license     * = "GPLv3-or-later"
-const author      * = "heysokam"
