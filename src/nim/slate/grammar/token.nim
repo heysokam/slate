ae171b6f0c047daee0d4b11b3b641c2f91008b40
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-25 14:57:29 +0200

    rmv: Delete Nim grammar tools sketch. Should use zig tools instead

--- src/nim/slate/grammar/token.nim
+++
@@ -1,7 +0,0 @@
-#:______________________________________________________________________
-#  grammar  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
-#:______________________________________________________________________
-
-type Id *{.pure.} = enum ident, Char
-type List * = seq[token.Id]
-
