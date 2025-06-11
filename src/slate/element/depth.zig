e33e0a12706a3728fc123f198ba1a88478227ead
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-11 11:07:45 +0200

    doc: Better doc-comments for `element.Depth`

--- src/slate/element/depth.zig
+++ src/slate/element/depth.zig
@@ -5,13 +5,14 @@ pub const Depth = @This();
 // @deps slate
 const source = @import("../source.zig");
 
-/// @descr Level used to decide what's in or out of scope
+/// @descr DepthLevel deciding what's in or out of scope
 scope   :Depth.Level,
 /// @descr
-///  Level used to decide increases/decreases in indentation.
-///  Used to for meaningful indentation tagging.
+///  DepthLevel deciding increases/decreases in indentation.
+///  For meaningful indentation tagging.
 indent  :Depth.Level,
 
 pub fn default () Depth { return Depth{.indent= 0, .scope= 0}; }
 
 pub const Level = source.Pos;
+
