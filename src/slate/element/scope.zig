1cdecdc461b257d7e36f67f295cd33e853b52342
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-12 21:25:23 +0200

    new: Better Scoping tools: `init`, `increase`
    TODO: decrease

--- src/slate/element/scope.zig
+++ src/slate/element/scope.zig
@@ -22,15 +22,29 @@ pub fn create (A :std.mem.Allocator) Scope { return Scope{
   .history    = slate.Depth.History{},
 };}
 
-pub fn destroy (S :*const Scope) void {
+pub fn destroy (S :*Scope) void {
   S.history.deinit(S.A);
   S.id_highest = Scope.Id.None;
   S.A          = undefined;
 }
 
-pub fn current (S :*Scope) slate.Depth { return S.history.items[S.history.items.len-1]; }
-pub fn id_next (S :*Scope) void {
+pub fn init (S :*Scope, indent :slate.Depth.Level) !void {
+  try S.history.append(S.A, slate.Depth{
+    .indent = indent,
+    .scope  = S.id_highest.next()
+  });
+  S.id_highest = S.current().scope;
+}
+
+pub fn id_next (S :*Scope) Scope.Id {
   S.id_highest = S.id_highest.next();
   return S.id_highest;
+}
+pub fn current (S :*Scope) slate.Depth { return S.history.items[S.history.items.len-1]; }
+pub fn increase (S :*Scope, indent :slate.Depth.Level) !void {
+  try S.history.append(S.A, slate.Depth{
+    .indent = indent,
+    .scope  = S.id_next()
+  });
 }
 
